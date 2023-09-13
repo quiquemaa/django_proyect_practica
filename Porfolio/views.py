bcf80a4b46aba956871731d05782277be1b0628f
Quique <quiquemaa8@gmail.com>
2023-09-13 14:15:00 +0200

    tercer commit

--- Porfolio/views.py
+++ Porfolio/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Proyecto
 
 def home(request):
     return render(request, 'home.html')
+
+def Project(request):
+    proyectos = Proyecto.objects.all()
+    return render(request, 'Proyectos/proyectos.html', {'proyectos':proyectos})
 
